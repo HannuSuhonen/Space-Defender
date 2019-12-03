02373889e5b3a2732eca862e73027a13bac2dabf
Hannu Suhonen <han.suhonen@icloud.com>
2019-12-03 13:32:34 +0200

    Added Multiple waves, deleted wave config & move speed from EnemyPathing.cs

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] WaveConfig waveConfig;
+    WaveConfig waveConfig;
     List<Transform> waypoints;
-    [SerializeField] float moveSpeed = 2f;
+    
     int waypointIndex = 0;
+    float enemyMoveSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        enemyMoveSpeed = waveConfig.GetEnemyMoveSpeed();
         waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].transform.position;
     }
@@ -21,12 +23,17 @@ public class EnemyPathing : MonoBehaviour
         EnemyMovement();
     }
 
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig; //this. refers to THIS entire class.  
+    }
+
     private void EnemyMovement()
     {
         if (waypointIndex <= waypoints.Count - 1)
         {
             var targetPosition = waypoints[waypointIndex].transform.position;
-            var movementThisFrame = moveSpeed * Time.deltaTime;
+            var movementThisFrame = enemyMoveSpeed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
 
             if (transform.position == targetPosition)
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        
+    }
+
+
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig) // Spawn multiple enemies on interval set on GetTimeBetweenSpawns -> Serialized in WaveConfig. 
+    {
+        for (int enemyCount = 0; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            var newEnemy = Instantiate(waveConfig.GetEnemyPrefab(),
+            waveConfig.GetWaypoints()[0].transform.position,
+            Quaternion.identity);
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+}
