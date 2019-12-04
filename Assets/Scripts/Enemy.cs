e41f5d1c6dcadf1772fcc2abc40b10964cc242f9
Hannu Suhonen <han.suhonen@icloud.com>
2019-12-04 16:49:51 +0200

    Updated damage

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -50,7 +50,7 @@ public class Enemy : MonoBehaviour
     private void ProcessHit(DamageDealer damageDealer)
     {
         health -= damageDealer.GetDamage();
-        //damageDealer.Hit();
+        damageDealer.Hit();
         if (health <= 0)
         {
             Destroy(gameObject);
